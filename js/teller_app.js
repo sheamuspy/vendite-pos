a19a0517421ba09ab9ac1df51ea486067148d689
sheamuspy <sheamuspy@gmail.com>
2015-10-28 17:11:18 +0000

    fix

--- js/teller_app.js
+++ js/teller_app.js
@@ -1,4 +1,4 @@
-/*global document, $, jQuery, sendRequest, cordova, addToTable, alert, getProducts, sync, window */
+/*global $, document, jQuery, sendRequest, cordova, addToTable, alert, getProducts, sync, window */
 
 var test_json = '{"status":0, "products":{"9782342":{"PRODUCT_ID":"4","PRODUCT_NAME":"ideal milk","PRODUCT_BARCODE":"9782342","PRODUCT_PRICE":"2"},"6433949":{"PRODUCT_ID":"5","PRODUCT_NAME":"kalyppo","PRODUCT_BARCODE":"6433949","PRODUCT_PRICE":"1.4"}}}';
 
@@ -142,13 +142,8 @@ function sync() {
         sendRequest(URL);
 
         window.localStorage.setItem('trans', '');
-    }*/
+    }
 
 
-}
-
-function test() {
-    "use strict";
-    addToTable(1234243);
-}
+}*/
 
